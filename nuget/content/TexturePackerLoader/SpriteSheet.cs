1c0bc1f7d6136a82d76ef09882d85b12227a2b0e
Mike S <GameKyuubi@users.noreply.github.com>
2016-11-08 02:31:29 +0900

    Change IDictionary to OrderedDictionary
    Adds Enumerability to the sprite sheet for easier/more dynamic animation/looping

--- nuget/content/TexturePackerLoader/SpriteSheet.cs
+++ nuget/content/TexturePackerLoader/SpriteSheet.cs
@@ -1,35 +1,42 @@
 namespace TexturePackerLoader
 {
-    using System.Collections.Generic;
+  using System.Collections.Generic;
+  using System.Collections.Specialized;
 
-    using Microsoft.Xna.Framework.Graphics;
+  using Microsoft.Xna.Framework.Graphics;
+  using System.Collections;
+  using System.Linq;
 
-    public class SpriteSheet
+  public class SpriteSheet
+  {
+    private readonly OrderedDictionary spriteList;
+    public SpriteSheet()
     {
-        private readonly IDictionary<string, SpriteFrame> spriteList;
-
-        public SpriteSheet()
-        {
-            spriteList = new Dictionary<string, SpriteFrame>();
-        }
-
-        public void Add(string name, SpriteFrame sprite)
-        {
-            spriteList.Add(name, sprite);
-        }
-
-        public void Add(SpriteSheet otherSheet)
-        {
-            foreach (var sprite in otherSheet.spriteList)
-            {
-                spriteList.Add(sprite);
-            }
-        }
-
-        public SpriteFrame Sprite(string sprite)
-        {
-            return this.spriteList[sprite];
-        }
-
+      spriteList = new OrderedDictionary();
     }
+
+    public void Add(string name, SpriteFrame sprite)
+    {
+      spriteList.Add(name, sprite);
+    }
+
+    public void Add(SpriteSheet otherSheet)
+    {
+      for (int i = 0; i < otherSheet.spriteList.Count; i++)
+      {
+        spriteList.Add(otherSheet.spriteList.Cast<DictionaryEntry>().ElementAt(i), otherSheet.spriteList[i]);
+      }
+    }
+
+    public SpriteFrame Sprite(string sprite)
+    {
+      return (SpriteFrame)this.spriteList[sprite];
+    }
+
+    public SpriteFrame Sprite(int index)
+    {
+      return (SpriteFrame)this.spriteList[index];
+    }
+
+  }
 }
